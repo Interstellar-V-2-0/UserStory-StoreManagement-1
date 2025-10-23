b45e2d5f055b26b587ef62e0ea235955958df683
ZamirE27 <zaesco2001@gmail.com>
2025-10-23 11:56:02 -0500

    First Commit

--- ClientManagement.Domain/Models/OrderStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace Clientmanagement.Domain.Models;
-
-public class OrderStatus
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    
-    public List<Order> Orders { get; set; } = new List<Order>();
-}
--- ClientManagement.Infrastructure/AppDbContext.cs
+++
@@ -1,6 +0,0 @@
-namespace Clientmanagement.Infrastructure;
-
-public class AppDbContext
-{
-    
-}
